2ab653980622fdd1badfc37e3d8649f4fb248e4b
JosipHorvat <josiph988@gmail.com>
2021-02-12 13:38:36 +0100

    First commit

---
+++ src/main/java/com/josip/passnegertransport/PassnegertransportApplication.java
@@ -0,0 +1,13 @@
+package com.josip.passnegertransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassnegertransportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassnegertransportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/josip/passnegertransport/PassnegertransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josip.passnegertransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassnegertransportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
