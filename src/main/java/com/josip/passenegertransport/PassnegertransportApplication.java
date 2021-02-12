99834ca6a34f28559bae2491c99978ec4efbf133
JosipHorvat <josiph988@gmail.com>
2021-02-12 14:09:04 +0100

    Connected to database with entity TestModel

--- src/main/java/com/josip/passenegertransport/PassnegertransportApplication.java
+++ src/main/java/com/josip/passenegertransport/PassnegertransportApplication.java
@@ -1,4 +1,4 @@
-package com.josip.passnegertransport;
+package com.josip.passenegertransport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/josip/passenegertransport/PassnegertransportApplicationTests.java
+++ src/test/java/com/josip/passenegertransport/PassnegertransportApplicationTests.java
@@ -1,4 +1,4 @@
-package com.josip.passnegertransport;
+package com.josip.passenegertransport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
